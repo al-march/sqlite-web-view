d25d7f0221708e64c1a88fb048c084fb7f7b8990
Alexander <valverin95@gmail.com>
2023-12-07 19:24:29 +0300

    refactor: as workspace

--- packages/viewer/tailwind.config.js
+++ packages/viewer/tailwind.config.js
@@ -2,15 +2,13 @@
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/@solsy/ui/**/*.{js,ts,jsx,tsx}"
+    "../../node_modules/@solsy/ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["business"],
   },
-}
-
+};
